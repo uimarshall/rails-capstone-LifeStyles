6dc4e7f6d2410a13e672b57b98c3cb62005d89c3
uimarshall <uimarshall@gmail.com>
2020-09-28 14:06:26 +0100

    Add tbale for Votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_181130) do
+ActiveRecord::Schema.define(version: 2020_09_28_125804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,4 +35,15 @@ ActiveRecord::Schema.define(version: 2020_09_27_181130) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_votes_on_article_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "votes", "articles"
+  add_foreign_key "votes", "users"
 end
