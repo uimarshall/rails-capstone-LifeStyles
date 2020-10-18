c0f154e8eadea39a0942c2b67858e1a3e2b4dbad
uimarshall <uimarshall@gmail.com>
2020-10-18 22:32:11 +0100

    Fix error for heroku deployment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,12 @@
 const { environment } = require("@rails/webpacker"); //already present
 const webpack = require("webpack");
 environment.plugins.append(
-	"Provide",
-	new webpack.ProvidePlugin({
-		$: "jquery",
-		jQuery: "jquery",
-		Popper: ["popper.js", "default"],
-	})
-); //already present
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
+
+module.exports = environment; //already present
