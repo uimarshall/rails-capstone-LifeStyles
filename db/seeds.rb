b7a1b91047bdf481ea4001bc0751a9f184128210
uimarshall <uimarshall@gmail.com>
2020-10-19 16:55:57 +0100

    seed data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create(name: 'Business', priority: 7)
+Category.create(name: 'Food', priority: 8)
+Category.create(name: 'Fitness', priority: 9)
+Category.create(name: 'Movies', priority: 10)
+Category.create(name: 'Relationships', priority: 11)
